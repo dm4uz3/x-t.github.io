541f410ea72c4a9d687653e010d1505850f62cd7
cmp <cmp@airmail.cc>
2018-09-14 18:54:26 +0300

    [PORT] Port blog

--- compile.sh
+++ compile.sh
@@ -6,3 +6,4 @@ prefix="src"
 awk -f compile.awk $prefix/index.html > index.html
 awk -f compile.awk $prefix/about.html > about.html
 awk -f compile.awk $prefix/source.html > source.html
+awk -f compile.awk blog/src_2018.html > blog/2018.html
